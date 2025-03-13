1eb272a419bfb9943be62b4562d05170e6fad4f1
Heder <hedergabrielsalessousa@gmail.com>
2025-03-13 20:22:20 -0300

    Revisão JS

---
+++ Aula 02/revisão.js
@@ -0,0 +1,42 @@
+/* Declarar Variáveis
+var x = 7 //Declaração de variável universal
+let y = 77 //variável só existe dentro de um contexto/bloco
+*/
+
+/* If Else
+if(x < 10 && y ==77){
+    console.log("Oi Pessoal")
+}else{
+    console.log("Encontrou")
+}
+*/
+
+/* Repetição
+for(var i=0; i<=10; i++){
+    console.log(i)
+}
+*/
+
+/* Array
+var lista = [4, 3, 2, 1, 8, 90, 56, 43] //criar vetor
+
+
+console.log("Tamanho:"+lista.length)//tamanho do vetor
+
+console.log("Vetor:"+lista[1])//vetor da posição especifica 
+
+lista.push("test") //adiciona valores
+lista.push("45") //adiciona valores
+
+lista.pop()//exclui o ultimo
+lista.shift()//exclui o primeiro
+
+console.log("Vetor:"+lista)//vetor
+*/
+
+/* Buscar dentro de Arrays
+var lista =[1,2,3,4,5,6,7,8,9,21, 32,41,56,432,890]
+var elemento = 7
+console.log(lista.indexOf(elemento))
+*/
+
